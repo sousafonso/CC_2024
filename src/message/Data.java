6f65bede35ee72fc1528178b404e3925e27d9616
Afonso Sousa <a104262@alunos.uminho.pt>
2024-10-30 00:14:46 +0000

    Data and Task

--- src/message/Data.java
+++ src/message/Data.java
@@ -1,5 +1,48 @@
 package message;
 
 public abstract class Data {
+    private String id;
+    private String type;
+    private String description;
+    private String value;
+    private String timestamp;
 
+    public Data(String id, String type, String description, String value, String timestamp) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+        this.value = value;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", description='" + description + '\'' +
+                ", value='" + value + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                '}';
+    }
 }
